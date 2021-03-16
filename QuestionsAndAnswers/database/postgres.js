20f722f8b93cbc87f0189c71098020a42a3fdb46
Chris McVey <chris.mcvey@gmail.com>
2021-03-16 11:11:29 -0700

    Succesfully connecting to postgres database

---
+++ QuestionsAndAnswers/database/postgres.js
@@ -0,0 +1,23 @@
+const Db = require('pg').Pool;
+const db = new Db({
+  user: 'mcvey',
+  password: 'drbohr13',
+  host: 'localhost',
+  database: 'qanda',
+  port: 5432,
+});
+
+const test = () => {
+  db.connect((err, client, release) => {
+    if (err) {
+      return console.error(`Can't get client`, err);
+    } else {
+      console.log('Connected to pool succesfully', client);
+      release()
+      console.log('Client released')
+    }
+  })
+}
+
+module.exports.test = test;
+
--- QuestionsAndAnswers/server/index.js
+++ QuestionsAndAnswers/server/index.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const db = require('../database/postgres.js');
 
 const app = express();
 const port = 3000;
@@ -8,6 +11,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
+  console.log('Serving GET request to "/"')
+  db.test();
   res.status(200).send('This works!')
 })
 
