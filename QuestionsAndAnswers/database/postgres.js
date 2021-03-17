d7e6acce4e5a3c0ec0182a5cbe425af44221a8b3
Chris McVey <chris.mcvey@gmail.com>
2021-03-16 18:43:54 -0700

    Dotenv set up

--- QuestionsAndAnswers/database/postgres.js
+++ QuestionsAndAnswers/database/postgres.js
@@ -1,10 +1,10 @@
 const Db = require('pg').Pool;
 const db = new Db({
-  user: 'mcvey',
-  password: 'drbohr13',
-  host: 'localhost',
-  database: 'qanda',
-  port: 5432,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
 });
 
 const test = () => {
