7175c1cc711a0bab63388d6b6c6c0f4bd07c3be6
Chris McVey <chris.mcvey@gmail.com>
2021-03-17 15:59:05 -0700

    Endpoints created and variables decoded

--- QuestionsAndAnswers/database/postgres.js
+++ QuestionsAndAnswers/database/postgres.js
@@ -1,20 +1,26 @@
 const Db = require('pg').Pool;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_DATABASE, DB_PORT } = process.env;
 const db = new Db({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  host: DB_HOST,
+  database: DB_DATABASE,
+  port: DB_PORT,
 });
 
-const test = () => {
+const test = (callback) => {
   db.connect((err, client, release) => {
     if (err) {
       return console.error(`Can't get client`, err);
     } else {
-      console.log('Connected to pool succesfully');
-      release()
-      console.log('Client released')
+      client.query('SELECT * FROM questions WHERE id < 4', (err, result) => {
+        release()
+        if (err) {
+          return callback(err, null);
+        } else {
+          return callback(null, result.rows);
+        }
+      })
     }
   })
 }
--- QuestionsAndAnswers/server/index.js
+++ QuestionsAndAnswers/server/index.js
@@ -12,9 +12,75 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   console.log('Serving GET request to "/"')
-  db.test();
-  res.status(200).send('This works!')
+  db.test((err, data) => {
+    res.status(200).send(data)
+  });
 })
+
+app.get('/qa/questions', (req, res) => {
+  console.log('Serving GET request for questions');
+  const product_id = req.query.question_id;
+  const whichPage = req.query.page || 1;
+  const count = req.query.count || 5;
+  res.status(200).send('In progress');
+})
+
+app.get('/qa/questions/:question_id/answers', (req, res) => {
+  console.log('Serving GET request for answers to a question');
+  const question_id = req.params.question_id;
+  const whichPage = req.query.page || 1;
+  const count = req.query.count || 5;
+  res.status(200).send('In progress')
+})
+
+app.post('/qa/questions', (req, res) => {
+  console.log('Serving POST request to add a question');
+  const body = req.body.body;
+  const name = req.body.name;
+  const email = req.body.email;
+  const product_id = req.body.prodcut_id;
+  res.status(200).send('In progress');
+})
+
+app.post('/qa/questions/:question_id/answers', (req, res) => {
+  console.log('Serving POST request to add an answer');
+  const product_id = req.params.question_id;
+  const body = req.body.body;
+  const name = req.body.name;
+  const email = req.body.email;
+  const photos = req.body.photos;
+  res.status(200).send('In progress');
+})
+
+app.put('/qa/questions/:question_id/helpful', (req, res) => {
+  console.log('Serving PUT request to mark a question helpful');
+  const product_id = req.params.question_id;
+  res.status(200).send('In progress');
+})
+
+app.put('/qa/questions/:question_id/report', (req, res) => {
+  console.log('Serving PUT request to report a question');
+  const product_id = req.params.question_id;
+  res.status(200).send('In progress');
+})
+
+app.put('/qa/answers/:answer_id/helpful', (req, res) => {
+  console.log('Serving PUT request to mark an answer helpful');
+  const answer_id = req.params.answer_id;
+  res.status(200).send('In progress');
+})
+
+app.put('/qa/answers/:answer_id/report', (req, res) => {
+  console.log('Serving PUT request to report an answer');
+  const answer_id = req.params.answer_id;
+  res.status(200).send('In progress');
+})
+
+
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Server is up and running on port ${port}!`)
